ab639c4159eca43ffc04ebb35a0b52a921739c50
OfirYaron <im.mr.rem@gmail.com>
2013-12-03 11:30:28 +0200

    management controller and basic entity model

---
+++ AOLHack/AOLHack.Site/Controllers/ManagementController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AOLHack.Site.Controllers
+{
+    public class ManagementController : Controller
+    {
+        //
+        // GET: /Management/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
